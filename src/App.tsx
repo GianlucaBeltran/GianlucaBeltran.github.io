552642824156d44e86682e1b6313a460ad159e92
GianlucaBeltran <gianlucabeltran@gmail.com>
2025-01-26 23:09:52 +0100

    second try for gh deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-import { NavLink } from "react-router";
+// import { useState } from "react";
+// import reactLogo from "./assets/react.svg";
+// import viteLogo from "/vite.svg";
+// import "./App.css";
+// import { NavLink } from "react-router";
 
 function App() {
-  return <>
-  </>;
+  return <></>;
 }
 
 export default App;
